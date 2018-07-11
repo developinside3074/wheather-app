d6c02b4f313063842d264684a72689f721c4e6f8
developinside3074 <developinside3074@gmail.com>
2018-07-11 18:11:51 -0500

    Se agregar las propTypes a WeatherData

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import { CLOUDY } from './../../../constans/weathers';
 import './styles.css';
 
 
-const WeatherData = () =>(
-    <div className='weatherDataCont'>
-        <WeatherTemperature temperature={20} weatherState={CLOUDY}/>
-        <WeatherExtraInfo humidity={80} wind={'10ms/s'}/>
-    </div>
-);
+const WeatherData = ({ data }) => {
+
+    const{ temperature, weatherState, humidity, wind} = data;
+    return (
+        <div className='weatherDataCont'>
+            <WeatherTemperature temperature={temperature} weatherState={weatherState} />
+            <WeatherExtraInfo humidity={humidity} wind={wind} />
+        </div>
+    );
+
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape ({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import { SUN } from './../../constans/weathers';
 import './styles.css';
+
+
+const data = {
+  temperature: 20,
+  weatherState: SUN,
+  humidity: 10,
+  wind: '10 m/s',
+};
 
 const WeatherLocation = () => (  
   <div className='weatherLocationCont'>
     <Location city={'Peru Lima!'}/>
-    <WeatherData/>
+    <WeatherData data ={ data }/>
   </div>
 );
 
